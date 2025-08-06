b12ac6dc63eb156e3c881fa49b65b50f642f0c03
arijit83sarkar <mail2arijit83@gmail.com>
2025-08-07 00:31:25 +0530

    initial commit - just appliction main

---
+++ src/main/java/com/raven/spring_boot_rabbitmq/SpringBootRabbitmqApplication.java
@@ -0,0 +1,14 @@
+package com.raven.spring_boot_rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRabbitmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRabbitmqApplication.class, args);
+        System.out.println("Spring Boot with RabbitMQ application has started......");
+    }
+
+}
---
+++ src/test/java/com/raven/spring_boot_rabbitmq/SpringBootRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raven.spring_boot_rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
